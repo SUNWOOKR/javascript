85d397c5c57cd456e9ad8c17948f0baf8fb7a3e2
선우 <111554630+SUNWOOKR@users.noreply.github.com>
2023-08-19 14:37:45 +0900

    23/08/19

---
+++ twonine.js
@@ -0,0 +1,6 @@
+const toBut = ["potato", "tomato", "pizza", "one"];
+
+console.log(toBuy);
+toBuy[2] = "water";
+console.log(toBuy);
+toBuy.push("one"); //배열 끝에 이 값을 추가한다. python에서의 append와 동일
---
+++ twts.js
@@ -0,0 +1,33 @@
+// function sayHello(nameOfPerson, age) {
+//   console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
+
+// sayHello("nicco", 10);
+// sayHello("lynn", 23);
+// sayHello("dal", 21);
+
+// function plus(a, b) {
+//   console.log(a + b);
+// }
+// function divide(a, b) {
+//   console.log(a / b);
+// }
+
+// plus(1, 2);
+// divide(10, 2);
+
+// function sayHello(){ 밖에서는 이형태로 function문을 쓰지만
+// 아래 처럼 object문 안에 있을 떄는 sayHello: function(){}형태로 function문을
+//쓴다
+
+// }
+
+// const player = {
+//   name: "nico",
+//   sayHello: function (otherPersonsName) {
+//     console.log("Hello" + otherPersonsName + "Nice to meet you");
+//   },
+// };
+
+// console.log(player.name);
+// player.sayHello("lynn");
